79f807437bc2a4606341b06995dd3e283b37bd4a
drwvermeulen <drw.vermeulen@student.avans.nl>
2020-03-10 09:09:40 +0100

    Test Github

--- app/src/main/java/com/example/lightyear_beroepsproduct3/MainActivity.java
+++ app/src/main/java/com/example/lightyear_beroepsproduct3/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private String joep = "Joep is de beste";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
